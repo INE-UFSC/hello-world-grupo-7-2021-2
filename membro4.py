d622fb6d91aa6d4e0987e16cb9478919c15e6c0d
Alien_Love <86534496+Alien-Love@users.noreply.github.com>
2021-11-01 02:32:01 -0300

    membro4.py

--- membro4.py
+++ membro4.py
@@ -1,2 +1,2 @@
 def hello():
-    return "Me chamo Vitor Vidal. Tenho 24 anos e sou de floripa. Gosto de música e máquinas de todos os tipos. Espero, nesta matéria, absorver e compartilhar conhecimento. 
+    return "Me chamo Vitor Vidal. Tenho 24 anos e sou de floripa. Gosto de música e máquinas de todos os tipos. Minha primeira experiência em programação foi na disciplina POO I. Espero, nesta matéria, absorver e compartilhar conhecimento. 
