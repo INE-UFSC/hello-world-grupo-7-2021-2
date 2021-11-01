5465e21c09a138792f6b0b0cb206a108576eecfe
Alien_Love <86534496+Alien-Love@users.noreply.github.com>
2021-11-01 02:28:41 -0300

    Add files via upload

---
+++ membro4.py
@@ -0,0 +1,2 @@
+def hello():
+    return "Me chamo Vitor Vidal. Tenho 24 anos e sou de floripa. Gosto de música e máquinas de todos os tipos. Espero, nesta matéria, absorver e compartilhar conhecimento. 
